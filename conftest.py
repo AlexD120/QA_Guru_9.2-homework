dc9bcb91e69859f2539ac2100849a41e7c89860f
AlexD120 <alexdavydov92@gmail.com>
2023-11-08 22:21:57 +0300

    Поправил разрешение экрана на существуещее

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ from selene.support.shared import browser
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def open_browser():
     browser.open('https://google.com')
     yield
@@ -10,8 +10,8 @@ def open_browser():
 
 @pytest.fixture(autouse=True)
 def browser_window():
-    browser.config.window_width = 1080
-    browser.config.window_height = 800
+    browser.config.window_width = 1024
+    browser.config.window_height = 768
 
 @pytest.fixture
 def no_result_search():
